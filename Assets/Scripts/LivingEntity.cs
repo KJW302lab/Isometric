f40a52e004c0d643ff0a474afa5acf0a45f71484
jiwoongKim <103028975+Sandwich1991@users.noreply.github.com>
2023-04-06 12:43:34 +0900

    플레이어 전사 추가

--- Assets/Scripts/LivingEntity.cs
+++ Assets/Scripts/LivingEntity.cs
@@ -1,10 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LivingEntity
+public class LivingEntity : MonoBehaviour
 {
-    public int Hp;
-    public int Ap;
-    public int Att;
+    protected string ClassName;
+    protected int Hp;
+    protected int Ap;
+    protected int Att;
+
+    protected void SetData(CharacterData data)
+    {
+        ClassName = data.className;
+        Hp = data.hp;
+        Ap = data.ap;
+        Att = data.att;
+    }
 }
